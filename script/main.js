1d45043a96f59bc80d66b5ce3516a57f9a89373c
Chengjiu Hong <cjhong@mail.sfsu.edu>
2017-03-03 18:57:53 -0800

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,28 +1,3 @@
-// var corgi = document.getElementById("corgi");
-// var golden = document.getElementById("golden");
-// var bunny = document.getElementById("bunny");
-//
-// corgi.addEventListener("click",showPic);
-// golden.addEventListener("click",showPic);
-// bunny.addEventListener("click",showPic);
-//
-// function showPic(){
-// var allImg = document.querySelectorAll("img");
-// for(var i = 0; i < allImg.length;i++){
-//   allImg[i].className = "hide";
-// }
-//
-//   //get the id name
-//   var picId = this.attributes["data-img"].value;
-//   var pic = document.getElementById(picId);
-//   if(pic.className === "hide"){
-//     pic.className = "";
-//   }
-//   else{
-//     pic.className = "hide";
-//   }
-// }
-
 var checkList = document.getElementById("checklist");
 var items = checklist.querySelectorAll("li");
 var inputs = checklist.querySelectorAll("input");
